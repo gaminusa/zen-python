2c0530fc4a822983b6c4f9d2f7b2667077424422
gabriella <gnurena@phantasia.pe>
2014-03-03 18:26:30 -0500

    v1.1

--- zen.py
+++ zen.py
@@ -1,19 +1,13 @@
 import urllib
 
 
-print ("Ingrese comando:")
-com = raw_input()
-
-if com == 'zen_python':
-	try:  
-    		f = urllib.urlopen("https://api.github.com/zen")  
-    		print f.read()  
-    		f.close()  
-	except HTTPError, e:  
-    		print "Ocurrio un error"  
-    		print e.code  
-	except URLError, e:  
-    		print "Ocurrio un error"  
-    		print e.reason  
-else:
-	print ("Comando invalido")
+try:  
+    f = urllib.urlopen("https://api.github.com/zen")  
+    print f.read()  
+    f.close()  
+except HTTPError, e:  
+    print "Ocurrio un error"  
+    print e.code  
+except URLError, e:  
+    print "Ocurrio un error"  
+    print e.reason  
